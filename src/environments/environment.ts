990ffec0c91a5ac2707d6097db5bdd71072fb669
elanza-cloudanddelivery <e.lanza@cloudanddelivery.es>
2025-07-11 10:38:34 +0200

    config evironments

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:5142/api',
+    apiTimeout: 30000,
+    
+    // Claves para localStorage - deben ser únicas
+    tokenKey: 'medical_inventory_token',
+    userKey: 'medical_inventory_user',
+    
+    // Otras configuraciones específicas
+    appName: 'Medical Inventory System',
+    version: '1.0.0'
+  };
